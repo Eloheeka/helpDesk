f6aa290539911af64b565dd5298fe3418985db57
Zion Eloheeka <m.eloheeka12@gmail.com>
2025-12-24 18:53:58 +0200

    Initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        {/* Tailwind CDN */}
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
+
