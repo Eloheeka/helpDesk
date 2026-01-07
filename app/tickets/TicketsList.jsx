e02800be08ec0c655288e8e877ca8906a1878730
Zion Eloheeka <m.eloheeka12@gmail.com>
2026-01-07 08:52:59 +0200

    Json db

--- app/tickets/TicketsList.jsx
+++ app/tickets/TicketsList.jsx
@@ -1,9 +1,23 @@
 async function getTickets() {
-  
+  const res = await fetch ('http://localhost:3000/api/tickets', {cache: 'no-store'});
+  return res.json();
 }
 
-export default function TicketsList() {
+export default  async function TicketsList() {
+  const tickets = await getTickets();
   return (
-    <div>TicketsList</div>
+    <>
+      {tickets.mao((ticket) => (
+        <div key={ticket.id} className="card my-5">
+          <h3>{ticket.title}</h3>
+          <p>{ticket.description.slice(0,200)}...</p>
+          <div className= {`pill ${ticket.priority}`}>
+            {ticket.priority} priority
+          </div>
+        </div>
+      ))}
+    
+    
+    </>
   )
 }
